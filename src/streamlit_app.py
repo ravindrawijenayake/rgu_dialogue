5a732ecb7279408c2e89cc61bec528dea474dc14
Ravindra Wijenayake <126754516+ravindrawijenayake@users.noreply.github.com>
2025-06-07 01:56:08 +0100

    test

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -95,13 +95,12 @@ for key in default_keys:
 
 # === Clear callback ===
 def clear_all():
-    # Clear all input and output session state keys
+    # Clear all except uploaded_file (which is special, so clear that too by resetting key)
     st.session_state['transcript'] = ''
     st.session_state['utterances'] = None
     st.session_state['summary'] = ''
     st.session_state['mermaid_diagram'] = ''
     st.session_state['uploaded_file'] = None
-    st.session_state['transcript_text_area'] = ''  # Explicitly clear text area
     # Set flag to trigger rerun
     st.session_state['clear_flag'] = True
 
